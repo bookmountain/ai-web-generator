4284d3c9039c53ff0504fa0321e9d0fc231a8e21
Book <bookmountain0222@gmail.com>
2026-02-24 08:43:40 +1030

    chore: format files

--- src/main/java/com/book/aiwebgenerator/config/CorsConfig.java
+++ src/main/java/com/book/aiwebgenerator/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("*");
+            .allowCredentials(true)
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("*");
     }
 }
--- src/main/java/com/book/aiwebgenerator/controller/HealthController.java
+++ src/main/java/com/book/aiwebgenerator/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.book.aiwebgenerator.controller;
 
+import com.book.aiwebgenerator.common.BaseResponse;
+import com.book.aiwebgenerator.common.ResultUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
     @GetMapping("/")
-    public String healthCheck() {
-        return "OK";
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
     }
 }
